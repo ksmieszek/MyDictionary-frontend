b2212fd1c798c7f8e0583c91d309a06f55337064
pyszaleq <buc704@gmail.com>
2021-02-06 07:57:56 +0100

    Recompose folder structure

---
+++ src/views/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class Root extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+                Almost before we knew it, we had left the ground.
+            </div>
+        );
+    }
+}
+
+export default Root;
